c304ede6a3fb869676f7752adb7a62548924d084
VaggelisTsesmelidakis <vaggelis.tsesmelidakis@gmail.com>
2020-02-09 21:30:11 +0000

    Changed some project settings in anticipation of the "Send to Unreal" Blender tool. Took out the initialization of the floats for the animal movement and safety distance out of the C++ code. Decided to set that in Blueprint. Had to add the meta tag of "BlueprintReadWrite". Small correction with the walk/gallop threshold value in ABP_Stag.

--- Source/Chrono/AnimalCharacter.cpp
+++ Source/Chrono/AnimalCharacter.cpp
@@ -10,9 +10,9 @@ AAnimalCharacter::AAnimalCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	MaxAnimalWalkSpeed = GetCharacterMovement()->MaxWalkSpeed; //Set the animal's max walk speed to equal that of default character movement component setting
-	MaxAnimalRunSpeed = 4 * MaxAnimalWalkSpeed; // Just an arbitrary multiple of default walk speed here; should be modified in Blueprint for each animal
-	MinSafetyDistance = 500; //Arbitrary choice for the minumum distance the animal will keep from our character
+	//MaxAnimalWalkSpeed = GetCharacterMovement()->MaxWalkSpeed; //Set the animal's max walk speed to equal that of default character movement component setting
+	//MaxAnimalRunSpeed = 3 * MaxAnimalWalkSpeed; // Just an arbitrary multiple of default walk speed here; should be modified in Blueprint for each animal
+	//MinSafetyDistance = 500; //Arbitrary choice for the minumum distance the animal will keep from our character
 
 }
 
--- Source/Chrono/AnimalCharacter.h
+++ Source/Chrono/AnimalCharacter.h
@@ -28,11 +28,11 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	UPROPERTY(EditDefaultsOnly, Category = "Animal Movement")
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Animal Movement")
 	float MaxAnimalWalkSpeed; // To be set by CharacterMovementComponent's max walk speed value initially
-	UPROPERTY(EditDefaultsOnly, Category = "Animal Movement")
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Animal Movement")
 	float MaxAnimalRunSpeed;
-	UPROPERTY(EditDefaultsOnly, Category = "Animal Behavior")
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Animal Behavior")
 	float MinSafetyDistance;
 
 
